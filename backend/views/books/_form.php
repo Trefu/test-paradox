7e6971df0d12585028b566a648262d176944c45f
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-09-27 20:18:49 -0300

    select2 added to _form in books view

--- backend/views/books/_form.php
+++ backend/views/books/_form.php
@@ -6,27 +6,48 @@ use common\models\base\Editorials;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
 /* @var $this yii\web\View */
 /* @var $model common\models\base\Books */
 /* @var $form yii\widgets\ActiveForm */
+
+
 ?>
+
 
 <div class="books-form">
 
-    <?php
-    ?>
+
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'epilogue')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'editorial_id')->dropDownList(ArrayHelper::map(Editorials::find()->all(), 'id', 'editorial_name'))
-    ?>
+    <?= $form->field($model, 'editorial_id')->widget(Select2::class, [
+        'data' => ArrayHelper::map(Editorials::find()->all(), 'id', 'editorial_name'),
+        'options' => ['placeholder' => Yii::t('app', 'Select editorial')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ]
+    ]) ?>
 
-    <?= $form->field($model, 'author_id')->dropDownList(ArrayHelper::map(Authors::find()->all(), 'id', 'author_name')) ?>
+    <?= $form->field($model, 'author_id')->widget(Select2::class, [
+        'data' => ArrayHelper::map(Authors::find()->all(), 'id', 'author_name'),
+        'options' => ['placeholder' => Yii::t('app', 'Select author')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ]
+    ]) ?>
 
-    <?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map(Categories::find()->all(), 'id', 'category_name')) ?>
+    <?= $form->field($model, 'category_id')->widget(Select2::class, [
+        'data' => ArrayHelper::map(Categories::find()->all(), 'id', 'category_name'),
+        'options' => ['placeholder' => Yii::t('app', 'Select category')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ]
+    ]) ?>
+
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
